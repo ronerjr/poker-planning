74c64bcde871020907b5bc1f1a52f9ed303a173e
juniorro <roner.junior@lhs-systems.com>
2017-08-16 09:43:09 -0300

    prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAWFlV5L7_iGwzsdUazIsfOODoabGCles0',
+    authDomain: 'pokerplanning-8a54c.firebaseapp.com',
+    databaseURL: 'https://pokerplanning-8a54c.firebaseio.com',
+    projectId: 'pokerplanning-8a54c',
+    storageBucket: 'pokerplanning-8a54c.appspot.com',
+    messagingSenderId: '606131909629'
+  }
 };
